c2f6359dcbf048e53cfcb23f077adf775603197f
DarkLite1 <BrechtGijbels@gmail.com>
2021-10-19 14:29:16 +0200

    added function Set-VolumeHC

--- Toolbox.General.psm1
+++ Toolbox.General.psm1
@@ -730,6 +730,113 @@ Function Remove-PowerShellWildcardCharsHC {
         $Name
     }
 }
+Function Set-VolumeHC {
+    <# 
+    .SYNOPSIS   
+        Control the volume of the speakers on a computer.
+
+    .DESCRIPTION
+        Set the audio volume, mute or un-mute speakers, have the computer
+        say something.
+
+    .PARAMETER ComputerName
+        Name of the computer where to set the volume
+
+    .PARAMETER Volume
+        The audio volume in percentage (0 is off and 50 is half way)
+
+    .PARAMETER TextToSay
+        Text that will be announced on the computer. This can be convenient
+        to test if the volume reached the desired setting.
+
+    .PARAMETER Mute
+        When this switch is used the audio volume is muted and you can't hear
+        anything on the system, despite the volume level being set.
+
+    .EXAMPLE
+        Set-VolumeHC -ComputerName PC1 -Volume 20 -TextToSay 'hi there' 
+
+        Set the audio volume to 20% on PC1 and say the text 'hi there'.
+
+    .EXAMPLE
+        Set-VolumeHC -ComputerName PC1 -Volume 70 -Mute
+
+        Set the audio volume to 70% on PC1 and mute the audio. All sound will be
+        blocked completely.
+    #>
+
+    Param (
+        [Parameter(Mandatory)]
+        [String[]]$ComputerName,
+        [Parameter(Mandatory)]
+        [ValidateRange(0, 100)]
+        [Int]$Volume,
+        [String]$TextToSay,
+        [Switch]$Mute
+    )
+
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        Add-Type -Language CSharpVersion3 -TypeDefinition @'
+    using System.Runtime.InteropServices;
+    [Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IAudioEndpointVolume
+    {
+        // f(), g(), ... are unused COM method slots. Define these if you care
+        int f(); int g(); int h(); int i();
+        int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+        int j();
+        int GetMasterVolumeLevelScalar(out float pfLevel);
+        int k(); int l(); int m(); int n();
+        int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+        int GetMute(out bool pbMute);
+    }
+    [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IMMDevice
+    {
+        int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+    }
+    [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    interface IMMDeviceEnumerator
+    {
+        int f(); // Unused
+        int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+    }
+    [ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+    public class Audio
+    {
+        static IAudioEndpointVolume Vol()
+        {
+            var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+            IMMDevice dev = null;
+            Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+            IAudioEndpointVolume epv = null;
+            var epvid = typeof(IAudioEndpointVolume).GUID;
+            Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+            return epv;
+        }
+        public static float Volume
+        {
+            get { float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v; }
+            set { Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty)); }
+        }
+        public static bool Mute
+        {
+            get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+            set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+        }
+    }
+'@
+    
+        [audio]::Mute = $using:Mute
+        [audio]::Volume = $using:Volume / 100
+    
+        if ($using:TextToSay) {
+            Add-Type -AssemblyName System.Speech
+            $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+            $speak.Speak($using:TextToSay)
+        }
+    }
+}
 Function Show-MenuHC {
     <#
     .SYNOPSIS
